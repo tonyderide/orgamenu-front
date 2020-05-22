953816429d8408e0979597b342b09855dfb13a46
tony deride <tony.deride.lp4@gmail.com>
2020-05-22 04:22:05 +0200

    preference fonctionnel
    reload du select-recette

---
+++ src/app/models/allergene.ts
@@ -0,0 +1,7 @@
+import {User} from "./user";
+
+export interface Allergenes {
+  id: number;
+  name: string;
+  users:User[];
+}
---
+++ src/app/models/preferenceAlim.ts
@@ -0,0 +1,4 @@
+export interface PreferenceAlim {
+  idPreferenceAliment?: number;
+  nomPreferenceAliment: string ;
+}
